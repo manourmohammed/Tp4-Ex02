c2c5387bc834ccc89e3d4c6b6bd26e8a02f0df61
med manour <manour12@gmail.com>
2023-11-28 00:30:21 +0100

    Task 2

--- app/src/main/java/com/example/tp4ex02/Activity1.java
+++ app/src/main/java/com/example/tp4ex02/Activity1.java
@@ -3,12 +3,20 @@ package com.example.tp4ex02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity1 extends AppCompatActivity {
+    Button btn1,btn2,btn3;
+    EditText ed1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        btn1=findViewById(R.id.btn_1);
+        btn2=findViewById(R.id.btn_2);
+        btn3=findViewById(R.id.btn_3);
+        ed1=findViewById(R.id.ed_number);
     }
 }
