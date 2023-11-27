ce460649e0007cde9f39d5a9372c176d904692c5
med manour <manour12@gmail.com>
2023-11-28 00:57:22 +0100

    Task 3 programmer la button quitter et reinitialisation

--- app/src/main/java/com/example/tp4ex02/Activity1.java
+++ app/src/main/java/com/example/tp4ex02/Activity1.java
@@ -2,14 +2,19 @@ package com.example.tp4ex02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity1 extends AppCompatActivity {
     Button btn1,btn2,btn3;
     EditText ed1;
+    TextView Text;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,18 @@ public class Activity1 extends AppCompatActivity {
         btn2=findViewById(R.id.btn_2);
         btn3=findViewById(R.id.btn_3);
         ed1=findViewById(R.id.ed_number);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ed1.setText(null);
+
+            }
+        });
+
+
+        btn3.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
