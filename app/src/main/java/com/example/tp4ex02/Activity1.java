d4ce135208db017bc4dc353ed850cbe5929fa674
med manour <manour12@gmail.com>
2023-11-28 03:02:02 +0100

    Task 8 pregramme la button afficher

--- app/src/main/java/com/example/tp4ex02/Activity1.java
+++ app/src/main/java/com/example/tp4ex02/Activity1.java
@@ -26,28 +26,37 @@ public class Activity1 extends AppCompatActivity {
         ed1=findViewById(R.id.ed_number);
         text=findViewById(R.id.text);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(v -> {
+            ed1.setText(null);
+            text.setText(
+                    "?*0=?" +
+                    "\n?*1=?" +
+                    "\n?*2=?" +
+                    "\n?*3=?" +
+                    "\n?*4=?" +
+                    "\n?*5=?" +
+                    "\n?*6=?" +
+                    "\n?*7=?" +
+                    "\n?*8=?" +
+                    "\n?*9=?");
+            if (ed1.getText().toString().isEmpty()){
+                Toast.makeText(Activity1.this, "vide", Toast.LENGTH_SHORT).show();
+            }
+
+
+       });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
             @Override
             public void onClick(View v) {
-                ed1.setText(null);
-                text.setText(
-                        "?*0=?" +
-                        "\n?*1=?" +
-                        "\n?*2=?" +
-                        "\n?*3=?" +
-                        "\n?*4=?" +
-                        "\n?*5=?" +
-                        "\n?*6=?" +
-                        "\n?*7=?" +
-                        "\n?*8=?" +
-                        "\n?*9=?");
-                if (ed1.getText().toString().isEmpty()){
-                    Toast.makeText(Activity1.this, "vide", Toast.LENGTH_SHORT).show();
+                int Number = Integer.parseInt(ed1.getText().toString());
+                StringBuilder s= new StringBuilder();
+                for (int i=1;i<=9;i++) {
+                    int result= Number*i;
+                    s.append(String.format(" %d*%d=%d \n", Number, i, result));
                 }
-
-
-           }
-
+                text.setText(s.toString());
+            }
         });
 
 
